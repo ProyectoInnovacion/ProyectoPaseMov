c7a8bafe068e99c81b7e9eb5cb86a5c26cbd2cf2
soladox <sebastianfuentesf@gmail.com>
2021-11-09 22:42:38 -0300

    Cambios a lectura de pdf

--- app/src/main/java/com/proyecto/pasemov/ui/home/HomeFragment.java
+++ app/src/main/java/com/proyecto/pasemov/ui/home/HomeFragment.java
@@ -3,6 +3,7 @@ package com.proyecto.pasemov.ui.home;
 import static android.app.Activity.RESULT_OK;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,28 +23,52 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.github.barteksc.pdfviewer.PDFView;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StreamDownloadTask;
 import com.google.firebase.storage.UploadTask;
 import com.proyecto.pasemov.R;
 import com.proyecto.pasemov.databinding.FragmentHomeBinding;
 import com.proyecto.pasemov.putPDF;
 
+import java.util.List;
+
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    ListView list;
+    PDFView pdfView;
+    Context thiscontext;
+    String url;
+    StorageReference storageRef;
+    FirebaseApp app;
+    FirebaseStorage storage;
+    FirebaseAuth auth;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        list=view.findViewById(R.id.lvPDF);
+        pdfView=view.findViewById(R.id.pdfPrincipal);
+        thiscontext=container.getContext();
+
 
         return view;
     }
+
 
 
     @Override
