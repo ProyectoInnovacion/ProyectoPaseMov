0e660deb884b7a968f359cb91938aae1b6a48abf
narutogemar12 <feliperayov@hotmail.com>
2021-11-09 22:27:25 -0300

    lo que hice ahora seria agregar mas contenido en fragment gallery que seria el boton que deciamos utilizar para despues hacer una buena libreta de informacion del usuario

--- app/src/main/java/com/proyecto/pasemov/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/proyecto/pasemov/ui/gallery/GalleryFragment.java
@@ -11,11 +11,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 import com.proyecto.pasemov.R;
 import com.proyecto.pasemov.databinding.FragmentGalleryBinding;
+import com.proyecto.pasemov.ui.gallery.controler.PagerController;
 
 public class GalleryFragment extends Fragment {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    TabItem tab1,tab2,tab3;
+    PagerController pagerAdapter;
 
     private GalleryViewModel galleryViewModel;
     private FragmentGalleryBinding binding;
@@ -28,9 +36,41 @@ public class GalleryFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        tabLayout= root.findViewById(R.id.tablayout);
+        viewPager= root.findViewById(R.id.viewpager);
+        tab1= root.findViewById(R.id.tabcontacto);
+        tab2= root.findViewById(R.id.tabcorreo);
+        tab3= root.findViewById(R.id.tabnotas);
+        pagerAdapter= new PagerController(getActivity().getSupportFragmentManager(), tabLayout.getTabCount() );
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if (tab.getPosition()==0){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+                if (tab.getPosition()==1){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+                if (tab.getPosition()==2){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+            }
 
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
 
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+            viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         return root;
+
     }
 
     @Override
---
+++ app/src/main/java/com/proyecto/pasemov/ui/gallery/controler/PagerController.java
@@ -0,0 +1,37 @@
+package com.proyecto.pasemov.ui.gallery.controler;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerController extends FragmentPagerAdapter {
+    public PagerController(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        this.numftabs = behavior;
+    }
+
+    int numftabs;
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new Contactos();
+            case 1:
+                return new Correos();
+            case 2:
+                return new Mensajes();
+            default:
+                return null;
+
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return numftabs;
+    }
+}
